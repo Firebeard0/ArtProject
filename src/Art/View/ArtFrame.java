b643a985c00439fcd3472f31ea17374e20f4679d
Noah Uffens <noah@uffens.com>
2018-05-23 23:13:55 -0600

    Made artframe and put in setup method

--- src/Art/View/ArtFrame.java
+++ src/Art/View/ArtFrame.java
@@ -1,12 +1,25 @@
 package Art.View;
+import Art.controller.*;
 
 import javax.swing.JFrame;
 
-public class ArtFrame  {
+public class ArtFrame extends JFrame {
+	
+	public ArtController ShapePanel;
 	
 	public ArtFrame()
 	{
 		super();
 		
+		this.ShapePanel = new ArtController();
+		setupFrame();
+	}
+	
+	void setupFrame()
+	{
+		this.setSize(1200,700);
+		this.setContentPane(appPanel);
+		this.setTitle("Creating modern art in Java");
+		this.setVisible(true);
 	}
 }
