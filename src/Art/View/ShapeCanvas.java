efe6de01c8480c3321ca3826cc0117dcae2724aa
Noah Uffens <noah@uffens.com>
2018-05-24 21:39:20 -0600

    made shapeCanvas

---
+++ src/Art/View/ShapeCanvas.java
@@ -0,0 +1,24 @@
+package Art.View;
+
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.util.*;
+import javax.imageio.*;
+import javax.swing.*;
+import Art.controller.*;
+
+public class ShapeCanvas extends JPanel{
+private ArrayList<Polygon> triangleList;
+private ArrayList<Polygon> polygonList;
+private ArrayList<Ellipse2D> ellipseList;
+private ArrayList<Rectangle> rectangleList;
+
+private BufferedImage canvasImage;
+
+public ShapeCanvas(ArtController app)
+{
+	
+}
+}
